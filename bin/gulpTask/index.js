39886b0b23ca35e12b4ab43d4076f217abe67d3e
qiushangzhe <qiusz@jiqdaibao.com>
2017-12-14 19:29:55 +0800

    delete

--- bin/gulpTask/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./initProject/init.task");
-const gulp = require("gulp");
-exports.task = {
-    run: (command) => {
-        gulp.start(command);
-    }
-};
