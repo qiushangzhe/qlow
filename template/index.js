2b21649db732c9bfd9604dac4d097563354329ec
邱上哲 <470542573@qq.com>
2020-05-28 15:39:30 +0800

    完成

--- template/index.js
+++
@@ -1,2 +0,0 @@
-import $ from 'zepto'
-import './src/styles/index.styl'
--- template/webpack.config.js
+++ template/webpack.config.js
@@ -2,27 +2,63 @@
  * webpack的配置
  */
 const { resolve } = require('path')
+const devMode = process.env.NODE_ENV !== 'production';
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPugPlugin = require('html-webpack-pug-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 module.exports = {
-  entry: resolve(__dirname, 'index.js'),
+  entry: [ resolve(__dirname, 'src/scripts/index.js') ],
   output: {
     filename: 'built.js',
     path: resolve(__dirname, 'build')
   },
   module: {
-    rules: [{
-      test: require.resolve('zepto'),
-      use: ['exports-loader?window.Zepto','script-loader']
-    }]
+    rules: [
+      {
+        oneOf: [
+          {
+            test: /\.styl$/,
+            use: [ 
+              devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+              'css-loader', 
+              { loader: 'postcss-loader', options: { ident: 'postcss', plugins: () => [ require('postcss-preset-env')() ] } },
+              'stylus-loader'
+            ]
+          },{
+            test: /\.(jpg|png|gif)/,
+            loader: 'url-loader',
+            options: { limit: 8 * 1024, name: '[hash:10].[ext]', outputPath: 'imgs', esModule: false }
+          },{
+            test: /\.html$/,
+            loader: 'html-loader',
+          },{
+            exclude: /\.(js|css|styl|html|jpg|png|gif|pug)/,
+            loader: 'file-loader',
+            options: { outputPath: 'media' }
+          },{
+            test: /\.pug$/,
+            loader: 'pug-loader'
+          }
+        ]
+      }],
   },
   plugins: [
-    new HtmlWebpackPlugin({ template: resolve(__dirname, 'src/index.html') })
+    new HtmlWebpackPlugin({ template: resolve(__dirname, 'src/index.pug'), minify: { collapseWhitespace: true, removeComments: true, } }),
+    new MiniCssExtractPlugin({ filename: "[name].css" }),
+    new OptimizeCssAssetsPlugin(),
+    new HtmlWebpackPugPlugin()
+
   ],
+  optimization: {
+    splitChunks: { chunks: 'all' }
+  },
   mode: 'development',
   devServer: {
     contentBase: resolve(__dirname, 'build'),
     compress: true,
-    open: true,
+    // open: true,
+    hot: true,
     port: 3000
   }
 }
